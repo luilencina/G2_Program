a090635bdd54b42721b1751cb4828ac18fba28b5
luilencina <luizalencina@hotmail.com>
2020-07-20 09:17:21 -0300

    error

--- src/Ex1.java
+++ src/Ex1.java
@@ -10,17 +10,17 @@ public class Ex1 {
     public static void main(String[] args) throws Exception {
         int n = 0;
 
-        try(Scanner console = new Scanner(System.in)){
+        try(Scanner in = new Scanner(System.in)){
             System.out.println("Este programa imprime o resultado serie definida por:");
             System.out.println("Somatorio de x^(2*j)/(2*j)+1 \ncom j variando de 0 até (n-1)");
             System.out.println("Com n inteiro, x real");
             System.out.println("Digite um n nao-positivo para sair");
             System.out.print("Digite o valor de n: ");
-            n = console.nextInt();
+            n = in.nextInt();
             while(n>0){
             StringBuilder sb = new StringBuilder();
             System.out.print("Digite o valor de x: ");
-            double x = console.nextDouble();
+            double x = in.nextDouble();
             System.out.print("\nSolucao Iterativa:");
             sb.append("S(").append(x).append(") com ").append(n).append(" termos = ").append(Series.iterativo(n, x));
             System.out.println(sb);
@@ -32,7 +32,7 @@ public class Ex1 {
             System.out.println("---------------");
             System.out.println("Digite um n nao-positivo para sair");
             System.out.println("\nDigite o valor de n: ");
-            n = console.nextInt();
+            n = in.nextInt();
             }
         }
         catch(InputMismatchException e){
